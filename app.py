7c90a64cec39a4d70ff7a0fbfe98ce1309010a22
Admin-HL <yeonghaoliang0822@gmail.com>
2024-06-04 14:35:20 +0800

    All syntaxes

--- app.py
+++ app.py
@@ -1,4 +1,82 @@
-first = float(input("First: "))
-second = float(input("Second: "))
-total = first + second
-print("Total: ", str(total))
+# first = float(input("First: "))
+# second = float(input("Second: "))
+# total = first + second
+# print("Total: ", str(total))
+
+# course = 'Python for Biginners'
+# print('for' in course)
+
+# x = 10
+# x*=3
+# print (x)
+
+# x = 3 != 2
+# print (x)
+
+# price = 25
+# print(price > 10 or price < 20)
+
+# temperature = 11
+# if temperature > 30:
+#     print("It's a hot day")
+#     print("Drink water")
+# elif temperature > 20:
+#     print ("It's a nice day")
+# elif temperature > 10:
+#     print ("It's a cold day")
+# else:
+#     print("Temperature undetermined")
+# print("Done")
+
+# weight = int(input("Weight : "))
+# conversion = input("K(g) or (L)bs: ")
+# if conversion.lower() == 'g':
+#     weight /= 0.45
+#     print("{:.2f}".format(weight))
+# else:
+#     print("{:.2f}".format(weight))
+# print("Conversion done.")
+
+# weight = int(input("Weight: "))
+# unit = input("(K)g or (L)bs: ")
+
+# if unit.upper() == "K":
+#     converted = weight * 2.20462  # converting kg to lbs
+#     print("Weight in Lbs: " + str(converted))
+# else:
+#     converted = weight * 0.453592  # converting lbs to kg
+#     print("Weight in Kgs: " + str(converted))
+
+# i = 1
+# while i <= 1_0: 
+#     print(i * '*')
+#     i = i+1
+
+# name = ["James", "Lim"]
+# name[0] = "Jon"
+# print(name[0:2])
+
+# nums = [0,1,2,3,4,5]
+# nums.insert(6,6)
+# print(len(nums))
+# nums.remove(5)
+# print(nums)
+# print(5 in nums)
+# print(len(nums))
+
+# nums = [1,2,3,4,5]
+# # for items in nums:
+# #     print (items)
+    
+# i = 0
+# while i < len(nums):
+#     print(nums[i])
+#     i = i + 1
+
+# numbers = range(5,10, 4)
+# for number in numbers:
+#     print(number)
+
+# numbers = (1,2,3)
+# numbers.count(2)
+
